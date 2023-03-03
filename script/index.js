5e6d8638c691dfc5db16ad6afb562fec22737898
zidhunath <zidhunath7@gmail.com>
2023-03-03 12:13:23 +0530

    landing page, registration page, jsonserver added

---
+++ script/index.js
@@ -0,0 +1,7 @@
+let adminButton= document.getElementById("adminBtn")
+let userButton = document.getElementById("userBtn")
+
+userButton .addEventListener("click",()=>{
+    console.log("clicked")
+    window.location.href=("registration.html")
+})
---
+++ script/regitsration.js
@@ -0,0 +1,11 @@
+let nameInput = document.getElementById("name");
+let ageInput = document.getElementById("age");
+let placeInput = document.getElementById("place");
+let profession = document.getElementById("profession");
+let form = document.getElementById("submit_Form");
+let submitButton = document.getElementById("submit")
+
+submitButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    
+})
